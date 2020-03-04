dbc58477eafdc5d763c386c21dda587aa73d0aca
Leo <>
2020-03-04 14:55:55 -0600

    4 marzo

--- codigosSillaGyro/moduloSensoresLaser/moduloSensoresLaser.ino
+++ codigosSillaGyro/moduloSensoresLaser/moduloSensoresLaser.ino
@@ -1,10 +1,20 @@
+
+#define sd1 5
+#define sd2 4
+#define sd3 3
+#define sd4 2
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(sd1, INPUT);
+  pinMode(sd2, INPUT);
+  pinMode(sd3, INPUT);
+  pinMode(sd4, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(map(analogRead(A5),0,1024,40,5));
-
+  Serial.println(analogRead(A1));
 }
